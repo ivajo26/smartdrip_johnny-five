3185f43d007081dd61290dd65ad298f7eaea69b4
ivajo26 <idiaz@devhunter.co>
2015-11-07 12:25:28 -0500

    inicio meteor.js

--- irridrip.js
+++ irridrip.js
@@ -18,6 +18,19 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // code to run on server at startup
+
+    console.log('server is ready !');
+    // Llamamos libreria e iniicamos la conexcion con el arduino
+    var five =  Meteor.npmRequire('johnny-five'),
+        board = new five.Board();
+
+    board.on("ready", function() {
+      //Iniciamos los sensores
+      var sensor = new five.Sensor("A0");
+      // Detectamos los cambios del sensor
+      sensor.on("change", function() {
+        console.log(this.value);
+      });
+    });
   });
 }
